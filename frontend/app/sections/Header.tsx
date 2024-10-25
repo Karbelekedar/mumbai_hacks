dc53c580cd004903be4c7dd68e915b79a14d90a5
rugved2802 <rugvedmairal28@gmail.com>
2024-10-25 23:54:39 +0530

    op1

---
+++ frontend/app/sections/Header.tsx
@@ -0,0 +1,31 @@
+import ArrowRight from "@/app/assets/arrow-right.svg";
+import Logo from "@/app/assets/logosaas.png";
+import MenuIcon from "@/app/assets/menu.svg";
+import Image from "next/image";
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0 backdrop-blur-sm z-20">
+
+      <div className="py-5">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            {/* Use Image for regular images */}
+            <Image src={Logo} alt="Saas Logo" height={40} width={40} />
+            {/* Use MenuIcon as a component directly */}
+            <MenuIcon className="h-5 w-5 md:hidden" />
+
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              {/* <a href="#">About</a>
+              <a href="#">Features</a>
+              <a href="#">Customers</a>
+              <a href="#">Updates</a>
+              <a href="#">Help</a> */}
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">Get for free</button>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
